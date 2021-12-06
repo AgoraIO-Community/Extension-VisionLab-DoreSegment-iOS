930205856f412f7c225972aabf61de5d99736567
Max Cobb <maxxc@mac.com>
2021-12-06 11:20:49 +0000

    bump minimum to v12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DoreSegment",
     defaultLocalization: "en",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "DoreSegment",
